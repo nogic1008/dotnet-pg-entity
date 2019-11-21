381ea735b60b57f55dff2e906673f462a54ac0ab
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2019-11-21 13:16:16 +0900

    init project

---
+++ src/TableEntityGenerator.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TableEntityGenerator.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/pg-entity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pg_entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
